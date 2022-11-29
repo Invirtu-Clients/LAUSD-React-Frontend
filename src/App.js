eea906e082631c551bdab5408cdbcca1c3247ec6
Devin Dixon <devindixon@Devins-MBP.lan>
2022-11-29 09:22:12 -0500

    Adding GA tracking code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 //import Template from "./templates/esports";
 import React from "react";
+import ReactGA from 'react-ga';
 import { lazy } from "react";
 
 let Template = null;
@@ -14,6 +15,10 @@ if (template_dir) {
   Template = require('./templates/error').default;
 }
 
+if(process.env.REACT_APP_GA_TRACKING_ID) {
+    ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
+    ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 function App() {
 
